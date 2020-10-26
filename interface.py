6fbe8b852887d282475e4ac3f73020271bdbff28
gageschmidt <gagemschmidt@gmail.com>
2020-10-26 00:14:27 -0500

    refactored a bit, added text container to insert addon list into

--- interface.py
+++ interface.py
@@ -1,36 +1,30 @@
-from pathlib import Path
-from tkinter import *
+from upkeep.installed import CheckAddons
+from tkinter import scrolledtext
+import tkinter as tk
 
 
-# static method to check for the standard addons folder for World Of Warcraft
-def check_for_addons():
-    addons_dir = Path('/Program Files (x86)/World of Warcraft/_retail_/Interface/AddOns')
-    if addons_dir.is_dir():
-        for sub_dir in addons_dir.iterdir():
-            # Use names to check for addon repos?
-            print(sub_dir.name)
-    else:
-        print('No addons currently installed, or the default path is not being used for your WoW Installation')
-
-
-class Window(Frame):
+class Window(tk.Frame):
 
     def _init_(self, master=None):
-        Frame.__init__(self, master)
+        tk.Frame.__init__(self, master)
         self.master = master
 
 
 # initialize tkinter
-root = Tk()
+root = tk.Tk()
 
 app = Window(root)
 
 root.wm_title('Ah! Real Addons')
 
-# static method, check for wow interface directory and any existing addons
+# instantiate empty, scrollable container for list of addons
+addon_check_status = tk.scrolledtext.ScrolledText(root, wrap=tk.WORD, width=40, height=10,
+                                                  font=("Times New Roman", 15))
+addon_check_status.grid(row=1, column=0, columnspan=2)
+
 # use the iteration of addon names to check for repos
-check_for_addons()
+# passing text container to insert the list of addons
+CheckAddons.check_for_addons(addon_check_status)
 
 # initialize window
 root.mainloop()
-
---
+++ upkeep/installed.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+
+# This class will be used to handle any of the various checks needed for currently installed addons
+class CheckAddons:
+
+    @staticmethod
+    # static method to check for the standard addons folder for World Of Warcraft
+    def check_for_addons(text_container):
+        addons_dir = Path('/Program Files (x86)/World of Warcraft/_retail_/Interface/AddOns')
+        if addons_dir.is_dir():
+            for sub_dir in addons_dir.iterdir():
+                addon_name = sub_dir.name
+                # Use names to check for addon repos?
+                text_container.insert("end-1c", addon_name + '\n')
+        else:
+            text_container.insert("end-1c", 'No addons currently installed, '
+                                            'or the default path is not being used for your WoW Installation')
